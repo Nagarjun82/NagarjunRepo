df8127285eb85be6df0dc947a766c22399c9b0a2
Nagarjun Edulakante <nagarjun.edulakante@gmail.com>
2018-09-14 22:51:44 -0400

    EOD_commit

--- Android_Apps/app/src/main/java/com/developer/arjun/arjunfirstapp/MainActivity.java
+++ Android_Apps/app/src/main/java/com/developer/arjun/arjunfirstapp/MainActivity.java
@@ -1,13 +1,22 @@
 package com.developer.arjun.arjunfirstapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void submitName(View view)
+    {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText)findViewById(R.id.UserNameField);
+        String userName = editText.getText().toString();
+        intent.putExtra("EXTRA_MESSAGE", userName);
+        startActivity(intent);
     }
 }
